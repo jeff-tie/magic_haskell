d25b13b78802ce3c61bf1e9f31f8b1845ffa3654
jeff_tie <liqitiesir@163.com>
2017-05-14 14:13:59 +0800

    chapter2/2.1.hs

---
+++ chapter2/2.1.hs
@@ -0,0 +1,17 @@
+data Position = MakePosition Double Double
+
+distance :: Position->Position->Double
+distance p1 p2 =
+    case p1 of
+        MakePosition x1 y1->
+            case p2 of
+                MakePosition x2 y2 -> sqrt((x1-x2)^2 + (y1-y2)^2)
+                
+pointA::Position
+pointA = MakePosition 0 0
+
+pointB::Position
+pointB = MakePosition 3 4
+
+main = do
+    print(distance pointA pointB)
